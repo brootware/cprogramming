d59f7c0d163c23173b482f88ad6c3fe94edca449
brootware <brootware@outlook.com>
2024-02-14 19:23:41 +0800

    added 1st program

---
+++ Helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int favNum = 0;
+    printf("Please enter your favorite number\n");
+    scanf("%d", &favNum);
+
+    printf("Your favorite number is %d\n",favNum);
+    return 0;
+}
